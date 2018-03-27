0d27532380b2aba61483d90d6a73884c9b258c40
Anthony Chi <a35931chi@gmail.com>
2018-03-26 20:54:34 -0400

    running LSTM testing in parallel

--- Model Implementation.py
+++ Model Implementation.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import time
 
+from sklearn.preprocessing import MinMaxScaler
+
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
@@ -16,7 +18,13 @@ df = pd.read_csv('Data/AAPL.csv', index_col = 'Date')
 #normalize
 df['Norm Adj Close'] = (df['Adj Close'] - df['Adj Close'].mean()) / (df['Adj Close'].max() - df['Adj Close'].min())
 df['Norm Vol'] = (df['Volume'] - df['Volume'].mean()) / (df['Volume'].max() - df['Volume'].min())
+
+#should compare normalization results with scaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+yahoo_stock_prices = scaler.fit_transform(df)
+
 #print(df.head())
+
 
 data_adjclose = np.array(df['Norm Adj Close'])
 data_adjvol = np.array(df['Norm Vol'])
@@ -171,3 +179,77 @@ if False: #plot results
     plt.legend(['original series', 'training fit', 'testing fit'])
     plt.tight_layout()
     plt.show()
+
+if False:
+    # let's try multiple layers
+    ind = []
+    training_error = []
+    testing_error = []
+    model_fit_time = []
+    training_predict_time = []
+    testing_predict_time = []
+    training_error_eval_time = []
+    testing_error_eval_time = []
+    window_sizes = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    epochs = [10, 100, 250, 500, 1000, 2000]
+    batch_sizes = [10, 50, 100, 250, 500]
+    for window_size in window_sizes:
+        for epoch in epochs:
+            for batch_size in batch_sizes:
+                X, y = window_transform_series(series = data_adjclose,
+                                               window_size = window_size)
+
+                # X.shape: (4576,7) and y.shape: (4576, 1)
+                # split our dataset into training / testing sets
+                train_test_split = int(np.ceil(2 * len(y) / float(3)))   # set the split point: 3051
+
+                X_train, y_train = X[:train_test_split, :], y[:train_test_split] #X_train.shape: (3051, 7), y_train: (3051, 1)
+                X_test, y_test = X[train_test_split:, :], y[train_test_split:] #x_test.shape: (1525, 7), y_test: (1525, 1)
+
+
+                # NOTE: to use keras's RNN LSTM module our input must be reshaped to [samples, window size, stepsize] 
+                X_train = np.asarray(np.reshape(X_train, (X_train.shape[0], window_size, 1))) #(3051, 7, 1)
+                X_test = np.asarray(np.reshape(X_test, (X_test.shape[0], window_size, 1))) #(1525, 7, 1)
+
+
+                model = Sequential()
+                model.add(LSTM(7, input_shape = (window_size, 1)))
+                model.add(Dense(1))
+
+                optimizer = keras.optimizers.RMSprop(lr = 0.001, rho = 0.9, epsilon = 1e-08, decay = 0.0)
+                model.compile(loss = 'mean_squared_error', optimizer = optimizer)
+                #print(model.summary())
+
+                t0 = time.time()
+                model.fit(X_train, y_train, epochs = epoch, batch_size = batch_size, verbose = 0)
+                t1 = time.time()
+
+                train_predict = model.predict(X_train)
+                t2 = time.time()
+                test_predict = model.predict(X_test)
+                t3 = time.time()
+
+                # evaluate training and testing errors
+                training_error = model.evaluate(X_train, y_train, verbose=0)
+                t4 = time.time()
+                testing_error = model.evaluate(X_test, y_test, verbose=0)
+                t5 = time.time()
+
+                print('\n')
+                print('window: {}, epoch: {}, batch size: {}'.format(window_size, epoch, batch_size))
+                print('training error = ' + str(training_error))
+                print('testing error = ' + str(testing_error))
+                print('model fit time: {0:.5g} s'.format(t1 - t0))
+                print('training predict time: {0:.5g} s'.format(t2 - t1))
+                print('testing predict time: {0:.5g} s'.format(t3 - t2))
+                print('training error eval time: {0:.5g} s'.format(t4 - t3))
+                print('testing error eval time: {0:.5g} s'.format(t5 - t4))
+
+                ind.append((window_size, epoch, batch_size))
+                training_error.append(training_error)
+                testing_error.append(testing_error)
+                model_fit_time.append(t1 - t0)
+                training_predict_time.append(t2 - t1)
+                testing_predict_time.append(t3 - t2)
+                training_error_eval_time.append(t4 - t3)
+                testing_error_eval_time.append(t5 - t4)
--- temp workspace.py
+++ temp workspace.py
@@ -23,7 +23,7 @@ np_aapl = scaler.fit_transform(df_aapl)
 df_aapl['Norm Adj Close'] = (df_aapl['Adj Close'] - df_aapl['Adj Close'].mean()) / (df_aapl['Adj Close'].max() - df_aapl['Adj Close'].min())
 df_aapl['Norm Vol'] = (df_aapl['Volume'] - df_aapl['Volume'].mean()) / (df_aapl['Volume'].max() - df_aapl['Volume'].min())
 
-if False:
+if False: #comparing two normalization techniques
     plt.plot(df_aapl[['Norm Adj Close']], label = 'calculated')
     plt.plot(np_aapl[:,4], label = 'scaler')
     plt.legend()
@@ -33,12 +33,24 @@ def window_transform_series_v2(series, window_size, days_out):
     # y is actually quite a few days into the future
     X = []
     y = []
+    y2 = []
     #print(series)
     #print(window_size)
     days_out = days_out - 1
     for i in range(len(series) - window_size - days_out):
         X.append(series[i: i + window_size])
         y.append(series[i + window_size + days_out])
+        diff = series[i + window_size + days_out] / series[i + window_size] - 1
+        if diff > 0.05:
+            y2.append(2)
+        elif diff > 0.01:
+            y2.append(1)
+        elif diff > -0.01:
+            y2.append(0)
+        elif diff > -0.05:
+            y2.append(-1)
+        else:
+            y2.append(-2)
         #print(i)
         #print(series[i: i + window_size])
         #print(series[i + window_size + days_out])
@@ -46,67 +58,95 @@ def window_transform_series_v2(series, window_size, days_out):
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
+    y2 = np.asarray(y2)
+    y.shape = (len(y), 1)
+    y2.shape = (len(y2), 1)
 
-    return X, y
+    return X, y, y2
 
 window_size = 5
 days_out = 5
 epoch = 500
 batch_size = 125
-X, y = window_transform_series_v2(np_aapl[:,4], window_size, days_out)
-print(X[:5])
-print(y[:5])
-print(np_aapl[:20, 4])
+X, y, y2 = window_transform_series_v2(np_aapl[:,4], window_size, days_out)
+#print(X[:5])
+#print(y[:5])
+#print(np_aapl[:20, 4])
 
 
 train_size = int(len(X) * 0.80)
 test_size = len(X) - train_size
 Xtrain, Xtest = X[0:train_size, :], X[train_size:len(X), :]
 ytrain, ytest = y[0:train_size, :], y[train_size:len(X), :]
+y2train, y2test = y2[0:train_size, :], y2[train_size:len(X), :]
 
 
 Xtrain = np.asarray(np.reshape(Xtrain, (Xtrain.shape[0], window_size, 1))) #
 Xtest = np.asarray(np.reshape(Xtest, (Xtest.shape[0], window_size, 1))) #
 
-print(Xtrain.shape, Xtest.shape)
+#print(Xtrain.shape, Xtest.shape)
 
-model = Sequential()
+ind = []
+training_errors = []
+testing_errors = []
+model_fit_time = []
+training_predict_time = []
+testing_predict_time = []
+training_error_eval_time = []
+testing_error_eval_time = []
 
-model.add(LSTM(input_dim = 1, output_dim = 50, return_sequences = True))
-model.add(Dropout(0.2))
+for first in [10, 50, 100, 200]:
+    for second in [10, 50, 100, 200]:
+        for dropout in [True, False]:
+            
+            model = Sequential()
 
-model.add(LSTM(100, return_sequences = False))
-model.add(Dropout(0.2))
+            model.add(LSTM(output_dim = first, input_shape = (window_size, 1),
+                           return_sequences = True))
+            if dropout:
+                model.add(Dropout(0.2))
 
-model.add(Dense(output_dim = 1))
-model.add(Activation('linear'))
+            model.add(LSTM(second, return_sequences = False))
+            if dropout:
+                model.add(Dropout(0.2))
 
-model.compile(loss = 'mse', optimizer = 'rmsprop')
+            model.add(Dense(output_dim = 1))
+            model.add(Activation('linear'))
 
-t0 = time.time()
-model.fit(Xtrain, ytrain, batch_size = batch_size, epoch = epoch,
-          validation_split = 0.05, verbose = 0)
-t1 = time.time()
+            model.compile(loss = 'mse', optimizer = 'rmsprop')
 
-train_predict = model.predict(Xtrain)
-t2 = time.time()
-test_predict = model.predict(Xtest)
-t3 = time.time()
+            t0 = time.time()
+            model.fit(Xtrain, ytrain, batch_size = batch_size, epochs = epoch,
+                      validation_split = 0.05, verbose = 0)
+            t1 = time.time()
 
-# evaluate training and testing errors
-training_error = model.evaluate(Xtrain, ytrain, verbose = 0)
-t4 = time.time()
-testing_error = model.evaluate(Xtest, ytest, verbose = 0)
-t5 = time.time()
+            train_predict = model.predict(Xtrain)
+            t2 = time.time()
+            test_predict = model.predict(Xtest)
+            t3 = time.time()
 
-print('\n')
-print('window: {}, epoch: {}, batch size: {}'.format(window_size, epoch, batch_size))
-print('training error = ' + str(training_error))
-print('testing error = ' + str(testing_error))
-print('model fit time: {0:.5g} s'.format(t1 - t0))
-print('training predict time: {0:.5g} s'.format(t2 - t1))
-print('testing predict time: {0:.5g} s'.format(t3 - t2))
-print('training error eval time: {0:.5g} s'.format(t4 - t3))
-print('testing error eval time: {0:.5g} s'.format(t5 - t4))
-print ('compilation time : ', time.time() - t0)
+            # evaluate training and testing errors
+            training_error = model.evaluate(Xtrain, ytrain, verbose = 0)
+            t4 = time.time()
+            testing_error = model.evaluate(Xtest, ytest, verbose = 0)
+            t5 = time.time()
+
+            print('\n')
+            print('window: {}, epoch: {}, batch size: {}'.format(window_size, epoch, batch_size))
+            print(first, second, dropout)
+            print('training error = ' + str(training_error))
+            print('testing error = ' + str(testing_error))
+            print('model fit time: {0:.5g} s'.format(t1 - t0))
+            print('training predict time: {0:.5g} s'.format(t2 - t1))
+            print('testing predict time: {0:.5g} s'.format(t3 - t2))
+            print('training error eval time: {0:.5g} s'.format(t4 - t3))
+            print('testing error eval time: {0:.5g} s'.format(t5 - t4))
+
+            ind.append((window_size, epoch, batch_size, first, second, dropout))
+            training_errors.append(training_error)
+            testing_errors.append(testing_error)
+            model_fit_time.append(t1 - t0)
+            training_predict_time.append(t2 - t1)
+            testing_predict_time.append(t3 - t2)
+            training_error_eval_time.append(t4 - t3)
+            testing_error_eval_time.append(t5 - t4)
