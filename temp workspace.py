bd606485ee9916ea9df8b852208d2f7b0392b7f3
Anthony Chi <a35931chi@gmail.com>
2018-03-27 21:30:08 -0400

    introducing more modeling results

--- temp workspace.py
+++ temp workspace.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 import time
 
-from sklearn.preprocessing import MinMaxScaler
+from scipy import stats
+from scipy.special import boxcox1p
+from scipy.stats import norm, skew
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 import keras
 from keras.models import Sequential
@@ -12,20 +16,25 @@ from keras.layers import Activation
 from keras.layers import Dropout
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #load prices
 df_aapl = pd.read_csv('Data/AAPL.csv', index_col = 'Date')
 
 #normalize
-scaler = MinMaxScaler(feature_range=(0, 1))
-np_aapl = scaler.fit_transform(df_aapl)
+SS_scaler = StandardScaler()
+SS_np_aapl = SS_scaler.fit_transform(df_aapl)
 
-df_aapl['Norm Adj Close'] = (df_aapl['Adj Close'] - df_aapl['Adj Close'].mean()) / (df_aapl['Adj Close'].max() - df_aapl['Adj Close'].min())
-df_aapl['Norm Vol'] = (df_aapl['Volume'] - df_aapl['Volume'].mean()) / (df_aapl['Volume'].max() - df_aapl['Volume'].min())
+MS_scaler = MinMaxScaler(feature_range=(0, 1))
+MS_np_aapl = MS_scaler.fit_transform(df_aapl)
+
+print('np_aapl')
+print('SS', SS_np_aapl[:20], np.min(SS_np_aapl))
+print('MS', MS_np_aapl[:20], np.min(MS_np_aapl))
 
 if False: #comparing two normalization techniques
-    plt.plot(df_aapl[['Norm Adj Close']], label = 'calculated')
-    plt.plot(np_aapl[:,4], label = 'scaler')
+    plt.plot(MS_np_aapl[:,4], label = 'MS scaler')
+    plt.plot(SS_np_aapl[:,4], label = 'SS scaler')
     plt.legend()
     plt.show()
 
@@ -33,52 +42,52 @@ def window_transform_series_v2(series, window_size, days_out):
     # y is actually quite a few days into the future
     X = []
     y = []
-    y2 = []
+    y_diff1 = []
+    y_diff2 = []
     #print(series)
     #print(window_size)
     days_out = days_out - 1
     for i in range(len(series) - window_size - days_out):
         X.append(series[i: i + window_size])
         y.append(series[i + window_size + days_out])
-        diff = series[i + window_size + days_out] / series[i + window_size] - 1
-        if diff > 0.05:
-            y2.append(2)
-        elif diff > 0.01:
-            y2.append(1)
-        elif diff > -0.01:
-            y2.append(0)
-        elif diff > -0.05:
-            y2.append(-1)
+        diff = series[i + window_size + days_out] / series[i + window_size - 1] - 1
+        y_diff1.append(diff)
+        if series[i + window_size - 1] == 0:
+            print(i, series[i + window_size + days_out], series[i + window_size - 1])
+            what = input('what')
+    
+        if diff > 0:
+            y_diff2.append(1)
         else:
-            y2.append(-2)
+            y_diff2.append(0)
+            
         #print(i)
         #print(series[i: i + window_size])
         #print(series[i + window_size + days_out])
-    # reshape each 
+    # reshape each
+
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
-    y2 = np.asarray(y2)
+    y_diff1 = np.asarray(y_diff1)
+    y_diff2 = np.asarray(y_diff2)
     y.shape = (len(y), 1)
-    y2.shape = (len(y2), 1)
+    y_diff1.shape = (len(y_diff1), 1)
+    y_diff2.shape = (len(y_diff2), 1)
 
-    return X, y, y2
+    return X, y, y_diff1, y_diff2
 
 window_size = 5
 days_out = 5
 epoch = 500
 batch_size = 125
-X, y, y2 = window_transform_series_v2(np_aapl[:,4], window_size, days_out)
-#print(X[:5])
-#print(y[:5])
-#print(np_aapl[:20, 4])
+X, y, y_diff1, y_diff2 = window_transform_series_v2(SS_np_aapl[:,4], window_size, days_out)
 
 
 train_size = int(len(X) * 0.80)
 test_size = len(X) - train_size
 Xtrain, Xtest = X[0:train_size, :], X[train_size:len(X), :]
 ytrain, ytest = y[0:train_size, :], y[train_size:len(X), :]
-y2train, y2test = y2[0:train_size, :], y2[train_size:len(X), :]
 
 
 Xtrain = np.asarray(np.reshape(Xtrain, (Xtrain.shape[0], window_size, 1))) #
@@ -95,25 +104,27 @@ testing_predict_time = []
 training_error_eval_time = []
 testing_error_eval_time = []
 
-for first in [10, 50, 100, 200]:
-    for second in [10, 50, 100, 200]:
-        for dropout in [True, False]:
+for dropout in [True, False]:
+    for first in [10, 50, 100, 200]:
+        for second in [10, 50, 100, 200]:
             
             model = Sequential()
 
             model.add(LSTM(output_dim = first, input_shape = (window_size, 1),
                            return_sequences = True))
             if dropout:
-                model.add(Dropout(0.2))
+                model.add(Dropout(0.1))
 
             model.add(LSTM(second, return_sequences = False))
+            
             if dropout:
-                model.add(Dropout(0.2))
+                model.add(Dropout(0.1))
 
             model.add(Dense(output_dim = 1))
-            model.add(Activation('linear'))
+            model.add(Activation('relu'))
 
-            model.compile(loss = 'mse', optimizer = 'rmsprop')
+            optimizer = keras.optimizers.RMSprop(lr = 0.001, rho = 0.9, epsilon = 1e-08, decay = 0.0)
+            model.compile(loss = 'mean_squared_error', optimizer = optimizer, metrics=['mse', 'accuracy'])
 
             t0 = time.time()
             model.fit(Xtrain, ytrain, batch_size = batch_size, epochs = epoch,
