1e5c65b9798887d8e6eeb55d7fa0bdcba1ee8bbf
Anthony Chi <a35931chi@gmail.com>
2018-03-29 15:49:24 -0400

    refined target distribution and transformation methodologies

--- Extract Data.py
+++ Extract Data.py
@@ -1,21 +1,20 @@
 import pandas as pd
+import numpy as np
 import pandas_datareader as pdr
 import datetime
 import quandl
+
+from scipy.stats import norm, skew
+from scipy import stats
+from scipy.special import boxcox1p
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #we got a problem...
 #yahoo and google API doesn't exist anymore. I'll need to pull updated info from quandl
 #and compare those with historical data downloaded from 
-
-quandl.ApiConfig.api_key = 'Jh3CAbmwaNP7YoqAN4FK'
-quandl.get_table('WIKI/PRICES', date='1999-11-18', ticker='A')
-
-def extract_from_yahoo(ticker):
-    data = pdr.get_data_yahoo(symbols = ticker,
-                              start = datetime(2000, 1, 1),
-                              end = datetime(2012, 1, 1))
-    return data
 
 def quandl_init(key):
     Quandlkey = ['7ieY8tq_kjzWx2-DiyGx', 'XrVxmKtfg2Fo3FG_NmtC', 'Jh3CAbmwaNP7YoqAN4FK']
@@ -46,31 +45,46 @@ def extract_from_quandl(ticker, start_date=(2000, 1, 1), end_date = None):
                       start_date = start_date, end_date = end_date,
                       collapse = 'daily', order = 'asc')
 
-if __name__ == '__main__':
-    tickers = ['HD', 'AAPL', 'FB']
-    '''
-    #get data from quandl
-    for ticker in tickers:
-        data = extract_from_quandl(ticker)
-        data.to_csv('Data/' + ticker + '-quandl.csv')
+def illustrate_datasets():
+    tickers = ['AAPL']
 
-    print('finished getting')
-    '''
-    '''
-    #comparing data sources (quandl vs. yahoo)
+    #get data from quandl, if necessary
+    if False:
+        for ticker in tickers:
+            data = extract_from_quandl(ticker)
+            data.to_csv('Data/' + ticker + '-quandl.csv')
+
+        print('finished getting from Quandl')
+
+    #comparing data sources (quandl vs. yahoo vs. NASDAQ)
     for ticker in tickers:
-        data_yahoo = pd.read_csv('Data/' + ticker + '.csv', index_col = 'Date')[['Adj Close', 'Volume']]
+        #create a base dataframe
+        df = pd.DataFrame(index = pd.date_range('2000/1/1', datetime.datetime.today(), freq='d'))
+        #load yahoo data, already downloaded from finance.yahoo.com refresh if necessary
+        data_yahoo = pd.read_csv('Data/' + ticker + '-yahoo.csv', index_col = 'Date')[['Adj Close', 'Volume']]
         data_yahoo.index = pd.to_datetime(data_yahoo.index)
+        #extract quandl data, extracted through API
         data_quandl = pd.read_csv('Data/' + ticker + '-quandl.csv', index_col = 'Date')[['WIKI/' + ticker + ' - Adj. Close', 'WIKI/' + ticker + ' - Adj. Volume']]
         data_quandl.index = pd.to_datetime(data_quandl.index)
+        #load NASDAQ data, already downloaded, refresh if necessary
+        data_nasdaq = pd.read_csv('Data/' + ticker + '-nasdaq.csv', index_col = 'date', dtype = {'volume': float})[['close','volume']]
+        data_nasdaq.index = pd.to_datetime(data_nasdaq.index)
+        data_nasdaq.index = data_nasdaq.index.date #gotta do this cuz nasdaq is weird
         
-        df = pd.DataFrame(index = pd.date_range('2000/1/1', '2018/3/23', freq='d'))
+        
         df = df.merge(data_yahoo[['Adj Close', 'Volume']], how = 'left', left_index = True, right_index = True)
         df = df.merge(data_quandl[['WIKI/' + ticker + ' - Adj. Close', 'WIKI/' + ticker + ' - Adj. Volume']], how = 'left', left_index = True, right_index = True)
-        df['Adj Close Diff'] = (df['Adj Close'] - df['WIKI/' + ticker + ' - Adj. Close']) / df['Adj Close']
-        df['Volume Diff'] = (df['Volume'] - df['WIKI/' + ticker + ' - Adj. Volume']) / df['Volume']
+        df = df.merge(data_nasdaq[['close', 'volume']], how = 'left', left_index = True, right_index = True)
+        #using the yahoo as benchmark
+        df['Adj Close Diff1'] = (df['Adj Close'] - df['WIKI/' + ticker + ' - Adj. Close']) / df['Adj Close']
+        df['Adj Close Diff2'] = (df['Adj Close'] - df['close']) / df['Adj Close']
+        df['Volume Diff1'] = (df['Volume'] - df['WIKI/' + ticker + ' - Adj. Volume']) / df['Volume']
+        df['Volume Diff2'] = (df['Volume'] - df['volume']) / df['Volume']
         df.dropna(how = 'all', axis = 0, inplace = True)
-        print(df[df['Volume Diff'].isnull()])
+        print(ticker)
+        print('length: ', len(df))
+        print('comparing yahoo and quandl, we are missing', sum(df['Volume Diff1'].isnull()), 'rows')
+        print('comparing yahoo and nasdaq, we are missing', sum(df['Volume Diff2'].isnull()), 'rows')
         
         #initializing the figure
         fig = plt.figure(figsize = (16, 8))
@@ -78,6 +92,7 @@ if __name__ == '__main__':
         ax1 = plt.subplot2grid((6,4),(0,0), rowspan = 4, colspan = 4)
         ax1.plot(df[['Adj Close']], label = 'yahoo', alpha = 0.5, color = 'r')
         ax1.plot(df[['WIKI/' + ticker + ' - Adj. Close']], label = 'quandl', alpha = 0.5, color = 'k')
+        ax1.plot(df[['close']], label = 'nasdaq', alpha = 0.5, color = 'g')
         ax1.legend()
         plt.ylabel('Price')
 
@@ -85,13 +100,16 @@ if __name__ == '__main__':
         ax2 = plt.subplot2grid((6,4),(4,0), sharex = ax1, rowspan = 1, colspan = 4)
         ax2.bar(df.index, df['Volume'], label = 'yahoo', alpha = 0.5, color = 'r')
         ax2.bar(df.index, df['WIKI/' + ticker + ' - Adj. Volume'], label = 'quandl', alpha = 0.5, color = 'k')
+        ax2.bar(df.index, df['volume'], label = 'nasdaq', alpha = 0.5, color = 'g')
         #ax2.axes.yaxis.set_ticklabels([]) #for some reason we are hiding the y axis lael for chart 2
         ax2.legend()
         plt.ylabel('Volume')
         
         ax3 = plt.subplot2grid((6,4),(5,0), sharex = ax1, rowspan = 1, colspan = 4)
-        ax3.plot(df[['Adj Close Diff']], label = 'close diff', alpha = 0.5, color = 'r')
-        ax3.plot(df[['Volume Diff']], label = 'vol diff', alpha = 0.5, color = 'k')
+        ax3.plot(df[['Adj Close Diff1']], label = 'close diff btw yho&quandl', alpha = 0.5, color = 'r')
+        ax3.plot(df[['Adj Close Diff2']], label = 'close diff btw yho&nasdaq', alpha = 0.5, color = 'g')
+        ax3.plot(df[['Volume Diff1']], label = 'vol diff btw yho&quandl', alpha = 0.5, color = 'k')
+        ax3.plot(df[['Volume Diff2']], label = 'vol diff btw yho&nasdaq', alpha = 0.5, color = 'b')
         #ax3.axes.yaxis.set_ticklabels([]) #for some reason we are hiding the y axis lael for chart 2
         ax3.legend()
 
@@ -102,35 +120,97 @@ if __name__ == '__main__':
 
         plt.xlabel('Date')
         
-        #plt.tight_layout()
+        plt.tight_layout()
         plt.subplots_adjust(left = 0.09, bottom = 0.18, right = 0.97, top = 0.94, wspace = 0.20, hspace = 0)
         plt.suptitle(ticker + ' Stock Price')
-        plt.setp(ax1.get_xticklabels(),visible = False) #removing the first chart's x label
+        plt.setp(ax1.get_xticklabels(), visible = False) #removing the first chart's x label
             
         plt.show()
-    '''
-    '''
-    # for some reason, the quandl datasets don't have data for some dates
-    2017-11-08 is a wednesday
-    2017-08-07 is a monday
+    pass
 
-                 Adj Close     Volume  WIKI/HD - Adj. Close  \
-    2017-11-08  162.297546  2912900.0                   NaN   
 
-                WIKI/HD - Adj. Volume  Adj Close Diff  Volume Diff  
-    2017-11-08                    NaN             NaN          NaN
+if __name__ == '__main__':
+    ticker = 'AAPL'
+    data_yahoo = pd.read_csv('Data/' + ticker + '-yahoo.csv', index_col = 'Date')
+    data_yahoo.index = pd.to_datetime(data_yahoo.index)
+    #here I'm trying to paint a shape of what's happening during the trading hours
+    data_yahoo['Open'] = data_yahoo['Open'] / data_yahoo['Close'] - 1
+    data_yahoo['High'] = data_yahoo['High'] / data_yahoo['Close'] - 1
+    data_yahoo['Low'] = data_yahoo['Low'] / data_yahoo['Close'] - 1
+    #this is what we are trying to predict
+    #1. 1 day future price, boxcox1p transform
+    #2. 5 days future price, boxcox1p transform
+    #3. 1 day future price percentage change
+    #4. 5 day future price percentage change
+    #5. 1 day future price direction
+    #6. 5 day future price direction
+    data_yahoo['Adj Close 1day'] = data_yahoo['Adj Close'].shift(-1)
+    data_yahoo['Adj Close 5day'] = data_yahoo['Adj Close'].shift(-5)
+    data_yahoo['Adj Close 1day pct_change'] = data_yahoo['Adj Close 1day'] / data_yahoo['Adj Close'] - 1
+    data_yahoo['Adj Close 5day pct_change'] = data_yahoo['Adj Close 5day'] / data_yahoo['Adj Close'] - 1
+    data_yahoo['Adj Close 1day pct_change cls'] = data_yahoo['Adj Close 1day pct_change'].apply(lambda x: 1 if x >= 0 else 0)
+    data_yahoo['Adj Close 5day pct_change cls'] = data_yahoo['Adj Close 5day pct_change'].apply(lambda x: 1 if x >= 0 else 0)
+    data_yahoo.dropna(axis = 0, how = 'any', inplace = True)
+    print(data_yahoo.head(10))
 
-                 Adj Close      Volume  WIKI/AAPL - Adj. Close  \
-    2017-08-07  156.982132  21870300.0                     NaN   
-    2017-11-08  174.895645  24409500.0                     NaN   
+    #let's look at the target variable distribution
+    #plt.hist(data_yahoo['Adj Close 1day']) or plt.hist(data_yahoo['Adj Close 5day']) don't show much
 
-                WIKI/AAPL - Adj. Volume  Adj Close Diff  Volume Diff  
-    2017-08-07                      NaN             NaN          NaN  
-    2017-11-08                      NaN             NaN          NaN
+    if False: #scaling isn't all that great for these two target variables
+        for col_label in ['Adj Close 1day', 'Adj Close 5day']:
+            #MM_Scaler = StandardScaler()
+            #data = MM_Scaler.fit_transform(data_yahoo[col_label])
+            #data = data - np.min(data)
+            data = data_yahoo[col_label]
+            lam = 0.0001
+            
+            fig, (ax1, ax2, ax3) = plt.subplots(ncols = 3, figsize = (15, 6))
+            sns.distplot(data, fit = norm, ax = ax1)
+            sns.distplot(boxcox1p(data, lam), fit = norm, ax = ax2)
+            sns.distplot(np.log(data + lam), fit = norm, ax = ax3)
+                
+            (mu1, sigma1) = norm.fit(data)
+            (mu2, sigma2) = norm.fit(boxcox1p(data, lam))
+            (mu3, sigma3) = norm.fit(np.log(data + lam))
+                
+            ax1.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu1, sigma1),
+                        'Skewness: {:.2f}'.format(skew(data))], loc = 'best')
+            ax2.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu2, sigma2),
+                        'Skewness: {:.2f}'.format(skew(boxcox1p(data, lam)))], loc = 'best')
+            ax3.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu3, sigma3),
+                        'Skewness: {:.2f}'.format(skew(np.log(data + lam)))], loc = 'best')
+            ax1.set_ylabel('Frequency')
+            ax1.set_title(col_label + ' Distribution')
+            ax2.set_title(col_label + ' Box-Cox Transformed')
+            ax3.set_title(col_label + ' Log Transformed')
+            plt.show()
 
-                 Adj Close      Volume  WIKI/FB - Adj. Close  \
-    2017-11-08  179.559998  10494100.0                   NaN   
-
-                WIKI/FB - Adj. Volume  Adj Close Diff  Volume Diff  
-    2017-11-08                    NaN             NaN          NaN  
-    '''
+    if False: #scalers doesn't really work here either
+        for col_label in ['Adj Close 1day pct_change', 'Adj Close 5day pct_change']:
+            lam = 0.0001
+            #MM_Scaler = MinMaxScaler()
+            #data = MM_Scaler.fit_transform(data_yahoo[col_label])
+            data = data_yahoo[col_label]
+            data = data - np.min(data)
+            
+            fig, (ax1, ax2, ax3) = plt.subplots(ncols = 3, figsize = (15, 6))
+            sns.distplot(data, fit = norm, ax = ax1)
+            sns.distplot(boxcox1p(data, lam), fit = norm, ax = ax2)
+            sns.distplot(np.log(data + lam), fit = norm, ax = ax3)
+                
+            (mu1, sigma1) = norm.fit(data)
+            (mu2, sigma2) = norm.fit(boxcox1p(data, lam))
+            (mu3, sigma3) = norm.fit(np.log(data + lam))
+                
+            ax1.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu1, sigma1),
+                        'Skewness: {:.2f}'.format(skew(data))], loc = 'best')
+            ax2.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu2, sigma2),
+                        'Skewness: {:.2f}'.format(skew(boxcox1p(data, lam)))], loc = 'best')
+            ax3.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu3, sigma3),
+                        'Skewness: {:.2f}'.format(skew(np.log(data + lam)))], loc = 'best')
+            ax1.set_ylabel('Frequency')
+            ax1.set_title(col_label + ' Distribution')
+            ax2.set_title(col_label + ' Box-Cox Transformed')
+            ax3.set_title(col_label + ' Log Transformed')
+            plt.show()
+    #let's look at the distribution between each independent and dependent variables
